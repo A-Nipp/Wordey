2ce4fae65b07034eb39adb9406a7aea214419c45
A-Nipp <65468669+A-Nipp@users.noreply.github.com>
2022-10-07 13:44:11 -0400

    changed to a text editor

--- Wordey/ContentView.swift
+++ Wordey/ContentView.swift
@@ -9,7 +9,43 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var rawText: String = "**Hi**"
+    @State private var isBold: Bool = false
     
+    
+    var body: some View {
+        ZStack {
+            Color.gray
+                .opacity(0.3)
+                .ignoresSafeArea()
+            VStack {
+                Text("Wordey")
+                    .font(.largeTitle)
+                VStack {
+                    HStack {
+                        Spacer()
+                        RenderedTextView(rawText: rawText)
+                            .padding()
+                        Spacer()
+                    }
+                }
+                .background(Color.white, in: RoundedRectangle(cornerRadius: 5))
+                Spacer()
+                TextEditor(text: $rawText)
+                Spacer()
+                VStack {
+                    Toggle(isOn: $isBold) {
+                        Text("Hi")
+                    }
+                }
+            }
+            .padding(.horizontal)
+        }
+    }
+    
+}
+
+struct RenderedTextView: View {
+    let rawText: String
     private var renderedText: AttributedString {
         do {
             let rendered = try AttributedString(
@@ -21,17 +57,12 @@ struct ContentView: View {
     }
     
     var body: some View {
-        Form {
-            Text(renderedText)
-            Section("Text Entry") {
-                TextField("Enter text", text: $rawText)
-            }
-        }
-    }
-    
+        Text(renderedText)
     
 }
 
+
+}
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
