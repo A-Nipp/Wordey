d4fa11a129909e8b39d9bbd3149b5c69ef0d6709
A-Nipp <65468669+A-Nipp@users.noreply.github.com>
2022-10-10 13:15:03 -0400

    Added segmented picker for color control

--- Wordey/ContentView.swift
+++ Wordey/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct ContentView: View {
     @State private var rawText: String = "**Hi**"
     @State private var isBold: Bool = false
-    @State private var textColor: Color = .primary
+    @State private var textColor: FontColor = .black
     @State private var textAlignment: HorizontalAlignment = .center
     
     var body: some View {
@@ -24,7 +24,7 @@ struct ContentView: View {
                     .bold()
                     .padding()
                 VStack {
-                    RenderedTextView(rawText: rawText)
+                    RenderedTextView(rawText: rawText, fontColor: textColor)
                         .frame(maxWidth: .infinity, maxHeight: .infinity)
                 }
                 .background(Color.white, in: RoundedRectangle(cornerRadius: 5))
@@ -48,6 +48,7 @@ struct ContentView: View {
 
 struct RenderedTextView: View {
     let rawText: String
+    let fontColor: FontColor
     private var renderedText: AttributedString {
         do {
             let rendered = try AttributedString(
@@ -60,6 +61,7 @@ struct RenderedTextView: View {
     
     var body: some View {
         var out = Text(renderedText)
+        out = out.foregroundColor(fontColor.getColor())
         return out
     
 }
@@ -74,7 +76,7 @@ struct ContentView_Previews: PreviewProvider {
 
 struct TextOptionsView: View {
     @Binding var isBold: Bool
-    @Binding var textColor: Color
+    @Binding var textColor: FontColor
     var body: some View {
         VStack(alignment: .leading) {
             Text("Options")
@@ -84,13 +86,46 @@ struct TextOptionsView: View {
                     Text("Hi")
                 }
                 Divider()
-                Toggle(isOn: $isBold) {
-                    Text("Hellow")
+                Picker("Please choose a color", selection: $textColor) {
+                    ForEach(FontColor.allCases, id: \.self) {
+                        Text($0.stringValue)
+                    }
                 }
+                .pickerStyle(.segmented)
             }
             .padding()
             .background(Color.white, in: RoundedRectangle(cornerRadius: 5))
             
         }
     }
+}
+
+enum FontColor: CaseIterable {
+    case white, black, blue, green
+    func getColor() -> Color {
+        switch self {
+        case .white:
+            return Color.white
+        case .black:
+            return Color.black
+        case .blue:
+            return Color.blue
+        case .green:
+            return Color.green
+        }
+    }
+    
+    var stringValue: String {
+        switch self {
+        case .white:
+            return "White"
+        case .black:
+            return "Black"
+        case .blue:
+            return "Blue"
+        case .green:
+            return "Green"
+        }
+    }
+    
 }
