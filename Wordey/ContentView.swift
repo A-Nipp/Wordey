68a6cf1e81b9c700c58de45d5911436777c46551
A-Nipp <65468669+A-Nipp@users.noreply.github.com>
2022-10-10 22:33:18 -0400

    finished font size slider, polished options view

--- Wordey/ContentView.swift
+++ Wordey/ContentView.swift
@@ -12,7 +12,9 @@ struct ContentView: View {
     @State private var isBold: Bool = false
     @State private var isItalicized: Bool = false
     @State private var textColor: FontColor = .black
-    @State private var textAlignment: TextAlignment = .center
+    @State private var hAlignment: HorizontalTextAlignment = .center
+    @State private var vAlignment: VerticalTextAlignment = .center
+    @State private var fontSize: CGFloat = 16
     
     var body: some View {
         ZStack {
@@ -25,9 +27,9 @@ struct ContentView: View {
                     .bold()
                     .padding()
                 VStack {
-                    RenderedTextView(rawText: rawText, fontColor: textColor, isBold: isBold, isItalicized: isItalicized)
+                    RenderedTextView(rawText: rawText, fontColor: textColor, isBold: isBold, isItalicized: isItalicized, fontSize: fontSize)
                         .padding()
-                        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: textAlignment.systemAlignment)
+                        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: Alignment(horizontal: hAlignment.systemAlignment, vertical: vAlignment.systemAlignment))
                 }
                 .background(Color.white, in: RoundedRectangle(cornerRadius: 5))
                 Spacer()
@@ -38,7 +40,7 @@ struct ContentView: View {
                 }
                 
                 Spacer()
-                TextOptionsView(isBold: $isBold, isItalicized: $isItalicized, textColor: $textColor, alignment: $textAlignment)
+                TextOptionsView(isBold: $isBold, isItalicized: $isItalicized, textColor: $textColor, vAlignment: $vAlignment, hAlignment: $hAlignment, fontSize: $fontSize)
                 
             }
             .padding(.horizontal)
@@ -48,37 +50,37 @@ struct ContentView: View {
     
 }
 
-enum TextAlignment: String, CaseIterable {
-    case topLeading = "Top Leading"
-    case top = "Top Center"
-    case topTrailing = "Top Trailing"
+enum HorizontalTextAlignment: String, CaseIterable {
     case leading = "Leading"
     case center = "Center"
     case trailing = "Trailing"
-    case bottomLeading = "Bottom Leading"
-    case bottom = "Bottom Center"
-    case bottomTrailing = "Bottom Trailing"
     
-    public var systemAlignment: Alignment {
+    public var systemAlignment: HorizontalAlignment {
         switch self {
-        case .topLeading:
-            return Alignment.topLeading // This is the same as all the ones below. Those work because Swift is expecting a return type of Alignment
-        case .top:
-            return .top
-        case .topTrailing:
-            return .topTrailing
         case .leading:
-            return .topTrailing
+            return .leading
         case .center:
             return .center
         case .trailing:
             return .trailing
-        case .bottomLeading:
-            return .bottomLeading
+        }
+    }
+}
+
+enum VerticalTextAlignment: String, CaseIterable {
+    case top = "Top"
+    case center = "Center"
+    case bottom = "Bottom"
+    
+    
+    public var systemAlignment: VerticalAlignment {
+        switch self {
+        case .top:
+            return .top
+        case .center:
+            return .center
         case .bottom:
             return .bottom
-        case .bottomTrailing:
-            return .bottomTrailing
         }
     }
 }
@@ -89,6 +91,7 @@ struct RenderedTextView: View {
     let fontColor: FontColor
     let isBold: Bool
     let isItalicized: Bool
+    let fontSize: CGFloat
     private var renderedText: AttributedString {
         do {
             let rendered = try AttributedString(
@@ -101,6 +104,7 @@ struct RenderedTextView: View {
     
     var body: some View {
         var out = Text(renderedText)
+        out = out.font(.system(size: fontSize))
         out = out.foregroundColor(fontColor.getColor())
         if isBold {
             out = out.bold()
@@ -124,18 +128,24 @@ struct TextOptionsView: View {
     @Binding var isBold: Bool
     @Binding var isItalicized: Bool
     @Binding var textColor: FontColor
-    @Binding var alignment: TextAlignment
+    @Binding var vAlignment: VerticalTextAlignment
+    @Binding var hAlignment: HorizontalTextAlignment
+    @Binding var fontSize: CGFloat
     var body: some View {
         VStack(alignment: .leading) {
             Text("Options")
                 .font(.subheadline)
             VStack {
-                Toggle(isOn: $isBold) {
-                    Text("Bold")
+                HStack {
+                    Toggle(isOn: $isBold) {
+                        Text("Bold")
+                    }
+                    Divider()
+                    Toggle(isOn: $isItalicized) {
+                        Text("Italics")
+                    }
                 }
-                Toggle(isOn: $isItalicized) {
-                    Text("Italics")
-                }
+                
                 Divider()
                 Picker("Please choose a color", selection: $textColor) {
                     ForEach(FontColor.allCases, id: \.self) {
@@ -144,12 +154,42 @@ struct TextOptionsView: View {
                 }
                 .pickerStyle(.segmented)
                 Divider()
-                Picker("Choose an alignment", selection: $alignment) {
-                    ForEach(TextAlignment.allCases, id: \.self) {
-                        Text($0.rawValue)
+                VStack(spacing: 2) {
+                    Text("Alignment")
+                    HStack {
+                        Text("Horizontal")
+                            .font(.footnote)
+                        Picker("Choose an alignment", selection: $hAlignment) {
+                            ForEach(HorizontalTextAlignment.allCases, id: \.self) {
+                                Text($0.rawValue)
+                            }
+                        }
+                        .pickerStyle(.menu)
+                        Text("Vertical")
+                            .font(.footnote)
+                        Picker("Choose an alignment", selection: $vAlignment) {
+                            ForEach(VerticalTextAlignment.allCases, id: \.self) {
+                                Text($0.rawValue)
+                            }
+                        }
+                        .pickerStyle(.automatic)
                     }
                 }
-                .pickerStyle(.menu)
+                Divider()
+                VStack(spacing: 5) {
+                    Text("Font Size")
+                    Slider(
+                        value: $fontSize,
+                        in: 1...100,
+                        step: 1
+                    ) {
+                        Text("Font Size")
+                    } minimumValueLabel: {
+                        Text("0")
+                    } maximumValueLabel: {
+                        Text("100")
+                    }
+                }
             }
             .padding()
             .background(Color.white, in: RoundedRectangle(cornerRadius: 5))
@@ -159,11 +199,11 @@ struct TextOptionsView: View {
 }
 
 enum FontColor: CaseIterable {
-    case white, black, blue, green
+    case black, blue, green, red
     func getColor() -> Color {
         switch self {
-        case .white:
-            return Color.white
+        case .red:
+            return Color.red
         case .black:
             return Color.black
         case .blue:
@@ -175,8 +215,8 @@ enum FontColor: CaseIterable {
     
     var stringValue: String {
         switch self {
-        case .white:
-            return "White"
+        case .red:
+            return "Red"
         case .black:
             return "Black"
         case .blue:
